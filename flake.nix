6c38163e106e132766faedfc767f98e0bf2b5d7c
Ricardo Antunes <me@riscadoa.com>
2024-10-23 15:46:47 +0200

    chore: setup nix environment

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: inputs.utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jdk22
+          maven
+          rlwrap
+        ];
+      };
+    });
+}
