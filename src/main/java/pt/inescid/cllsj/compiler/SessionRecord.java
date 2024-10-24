d0267be8598bc9da830fe34b147ea25a12ee4224
Ricardo Antunes <me@riscadoa.com>
2024-10-24 15:02:12 +0200

    feat(compiler): implement close, wait, par, cut

---
+++ src/main/java/pt/inescid/cllsj/compiler/SessionRecord.java
@@ -0,0 +1,14 @@
+package pt.inescid.cllsj.compiler;
+
+import pt.inescid.cllsj.ast.nodes.ASTNode;
+import pt.inescid.cllsj.ast.nodes.ASTType;
+
+public class SessionRecord {
+  public ASTType type;
+  public ASTNode continuation;
+
+  public SessionRecord(ASTType type, ASTNode continuation) {
+    this.type = type;
+    this.continuation = continuation;
+  }
+}
