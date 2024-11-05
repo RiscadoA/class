45329a136dc9c2a4bc1a926bfeaa35a00fd6a2aa
Ricardo Antunes <me@riscadoa.com>
2024-11-05 19:33:45 +0100

    build(compiler): add optional debug flag to compile-and-run.sh

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -4,11 +4,23 @@ cfile=$(mktemp $(basename $1).XXXXX.c)
 pfile=$(mktemp $(basename $1).XXXXX)
 trap 'rm -f $cfile $pfile' EXIT
 
+if [ $# -eq 2 ] && [ $2 = "-d" ]; then
+    flags="-g"
+    debug="yes"
+else
+    flags=""
+    debug="no"
+fi
+
 echo "@@@@@@ Running the code generator..." &&
 ./CLLSj -c < $1 > $cfile &&
 echo "@@@@@@ Generated C code:" &&
 cat $cfile &&
 echo "@@@@@@ Compiling the C code..." &&
-gcc -o $pfile $cfile &&
+gcc $flags -o $pfile $cfile &&
 echo "@@@@@@ Running the executable:" &&
-./$pfile
+if [ $debug = "yes" ]; then
+    gdb ./$pfile
+else
+    ./$pfile
+fi
