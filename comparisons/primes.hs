62867cc59968f16659d8ddcc4b1ca6ab16cf3c9a
Luis Caires <lcaires@fct.unl.pt>
2025-08-23 23:21:04 +0100

    sieve haskell

---
+++ comparisons/primes.hs
@@ -0,0 +1,39 @@
+module Main where
+
+data MyList a = Empty | LCons a (MyList a)
+  deriving (Show)
+
+intsfm k = LCons k (intsfm (k+1))
+
+filterp (LCons n xs) p = if (rem n p == 0)
+                     then filterp xs p
+                     else LCons n (filterp xs p)
+
+sievep (LCons p xs) = LCons p (sievep (filterp xs p))
+
+sieve (p:xs) = p : sieve [x | x <- xs, rem x p > 0]
+primes = sieve [2..]
+
+primesp = sievep (intsfm 2)
+
+printAll :: Show a => [a] -> IO ()
+printAll []     = return ()
+printAll (x:xs) = do
+  print x
+  printAll xs
+
+printAllp :: Show a => (MyList a) -> Int -> IO ()
+printAllp xs k   =
+          if (k==0)
+             then do
+                  print "DONE."
+                  return ()
+             else case xs of
+                       Empty -> return ()
+                       LCons x xs -> do
+                                print x
+                                printAllp xs (k-1)
+
+
+main :: IO ()
+main = printAllp (primesp) 10000
